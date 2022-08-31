3ae1f2d985ebc3f10a95baab693d720e3301bfb1
Weronika Utrata <werutr@wp.pl>
2022-08-31 11:40:30 +0200

    currencies added

---
+++ src/currencies.js
@@ -0,0 +1,5 @@
+export const currencies = [
+    { short: "EUR", name: "Euro", rate: 4.7459 },
+    { short: "USD", name: "Dolar amerykański", rate: 5.6769 },
+    { short: "GBP", name: "Funt brytyjski", rate: 4.3125 },
+];
