f5297af790b33d353b0a5eba5f8bc60ac3a09f36
Weronika Utrata <werutr@wp.pl>
2022-09-23 13:01:21 +0200

    styled js added to Container component

--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,7 +1,9 @@
-.container {
+import styled from "styled-components";
+
+export const StyledContainer = styled.main`
     max-width: 700px;
     margin: 0 auto;
     padding: 20px;
     font-family: 'Lato', sans-serif;
     font-size: large;
-}
+`;
